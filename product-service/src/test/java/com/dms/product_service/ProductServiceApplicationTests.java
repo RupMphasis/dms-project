76c341a89533bd350a422aebf70efbc08b436ee7
Rupesh Singh <rupesh.singh@mphasis.com>
2026-03-24 10:24:13 +0530

    Initial commit

---
+++ product-service/src/test/java/com/dms/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
